5666f346960294dee3a4dd59f2538e9386cc9e87
Keisei Akiya <keisei.akiya@gmail.com>
2025-03-19 01:46:01 +0900

    lazyvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,14 +1,2 @@
--- expandtab option. タブ文字をスペースに変換
-vim.cmd("set expandtab")
-
--- tabstop obtion. タブ文字の幅を4スペースに設定
-vim.cmd("set tabstop=4")
-
--- softtabstop option. insert mode でのタブの幅を4スペースに設定
-vim.cmd("set softtabstop=4")
-
--- shiftwidth option. indentに使用するスペースの幅を4スペースに設定
-vim.cmd("set shiftwidth=4")
-
--- jj to esc
-vim.api.nvim_set_keymap('i', 'jj', '<ESC>', { noremap = true, silent = true })
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
