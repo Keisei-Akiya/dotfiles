0ffd124e7a4e5862c480d2ca0fce847838e039e4
Keisei Akiya <keisei.akiya@gmail.com>
2025-03-19 14:57:08 +0900

    zshのVIMのモードによってカーソルの形が変わるようにした

---
+++ .zsh/alias.zsh
@@ -0,0 +1,21 @@
+# 2つ上、3つ上にも移動できるようにする
+alias ...='cd ../..'
+alias ....='cd ../../..'# Created by newuser for 5.8.1
+
+# for editor
+alias vi='nvim'
+alias vim='nvim'
+
+# language
+alias pip='pip3'
+alias python='python3'
+
+# command line tools
+alias cat='batcat'
+alias curl='curlie'
+alias grep='rg'
+alias ls='eza'
+alias ll='eza -l'
+alias la='eza -la'
+alias ps='procs'
+
---
+++ .zsh/extras.zsh
@@ -0,0 +1,64 @@
+# Vim キーバインドを有効にする
+bindkey -v
+# Add "jj" shortcut to enter NOMAL mode
+bindkey -M viins 'jj' vi-cmd-mode
+# カーソルの形
+function zle-line-init() {
+    if [[ $KEYMAP == vicmd ]]; then
+      printf '\e[1 q'  # 太いカーソル
+    else
+      printf '\e[0 q'  # 細いカーソル (|)
+    fi
+}
+
+function  zle-keymap-select() {
+    if [[ $KEYMAP == vicmd ]]; then
+      printf '\e[1 q'  # 太いカーソル
+    else
+      printf '\e[0 q'  # 細いカーソル
+    fi
+}
+
+zle -N zle-line-init
+zle -N zle-keymap-select
+
+autoload -U compinit; compinit # 自動補完を有効にする
+setopt correct # 誤字を訂正する (正しい可能性のある候補を表示)
+setopt correctall # 引数のスペルを修正
+setopt hist_ignore_dups # 連続する同じコマンドはヒストリに保存しない
+setopt share_history # ターミナル間でヒストリを共有する
+setopt inc_append_history # 即座にヒストリに追加
+
+# 入力したコマンドが存在せず，かつディレクトリ名と一致するなら，ディレクトリにcdする
+setopt auto_cd
+
+# history の設定
+export HISTFILE=~/.zsh_history
+export HISTSIZE=10000
+export SAVEHIST=10000
+
+# プラグイン設定
+plugins=(git zsh-autosuggestions zsh-syntax-highlighting)
+
+autoload -Uz compinit && compinit -u
+
+# プロンプト
+PROMPT="
+%{${fg[magenta]}%}[%n@%m]%{${reset_color}%} %~
+$ "
+
+# uv
+export PATH="$HOME/.local/bin:$PATH"
+eval "$(uv generate-shell-completion zsh)"
+eval "$(uvx --generate-shell-completion zsh)"
+
+# homebrew
+export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+# zsh-syntax-highlighting
+source /home/linuxbrew/.linuxbrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+# command line tools
+eval "$(zoxide init zsh)"
+
