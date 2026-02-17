9f67bc7dcc1545359c19b19cdbed9881569c5696
Keisei Akiya <keisei.akiya@gmail.com>
2026-02-17 22:51:29 +0900

    fish

---
+++ .config/fish/conf.d/abbr.fish
@@ -0,0 +1,14 @@
+# command line tools
+abbr -a cat bat
+abbr -a curl curlie
+abbr -a grep rg
+abbr -a ls eza
+abbr -a ll eza -l
+abbr -a la eza -la
+abbr -a ps procs
+
+# nvim
+abbr -a vi nvim
+
+# lazygit
+abbr -a lg lazygit
