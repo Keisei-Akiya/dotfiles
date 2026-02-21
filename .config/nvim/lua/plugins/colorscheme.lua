f21795631b5209c765d189f3c869c8d64b8f8935
Keisei Akiya <keisei.akiya@gmail.com>
2026-02-21 15:34:10 +0900

    nightfox carbonfox

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,4 @@
 return {
-  "craftzdog/solarized-osaka.nvim",
-  lazy = true,
-  priority = 1000,
-  opts = function()
-    return {
-      transparent = true,
-    }
-  end,
+  { "EdenEast/nightfox.nvim" },
+  { "LazyVim/LazyVim", opts = { colorscheme = "carbonfox" } },
 }
