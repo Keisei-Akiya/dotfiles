6616a62c7688b54d1bd9796e23951cbf1bc86171
Keisei-Akiya <keisei.akiya@gmail.com>
2026-02-23 02:03:41 +0900

    fish: ctrl e

--- .config/fish/conf.d/bind.fish
+++ .config/fish/conf.d/bind.fish
@@ -5,6 +5,11 @@ fish_vi_key_bindings
 function fish_user_key_bindings
     # jk で Esc
     bind -M insert -m default jk backward-char
+    # Ctrl + e で行末まで補完を確定（これも便利）
+    bind -M insert \ce accept-autosuggestion
+    # Ctrl + p / Ctrl + n で履歴検索（Vimモードでも使い慣れたキーを維持）
+    bind -M insert \cp up-or-search
+    bind -M insert \cn down-or-search
 
     # H, L で行頭・行末
     bind -M default H beginning-of-line
